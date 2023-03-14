f50c3cfe3b3be71c6d76325f1733decc89a720f4
ddotx_mbp <domemacintosh@gmail.com>
2023-03-14 23:02:41 +0700

    update

--- script/setenv.ts
+++ script/setenv.ts
@@ -1,1 +1,27 @@
-console.log(process.env)
+const { writeFile } = require('fs');
+
+// read environment variables from .env file
+require('dotenv').config();
+
+
+const targetPath = `./src/environments/environment.ts`;
+// we have access to our environment variables
+// in the process.env object thanks to dotenv
+const environmentFileContent = `
+export const environment = {
+  production: true,
+  API_ID: "${process.env.API_ID}"
+};
+`;
+
+console.log('---process.env---', process.env)
+
+console.log(environmentFileContent);
+// write the content to the respective file
+// @ts-ignore
+writeFile(targetPath, environmentFileContent, function (err) {
+  if (err) {
+      console.log(err);
+  }
+  console.log(`Wrote variables to ${targetPath}`);
+});
